b1d45e0f783fa84fc2233830b5d1311f75a2c68b
Petho Alpar <Petho Alpar>
2017-02-26 13:22:26 +0200

    Permission require included

--- app/src/main/java/com/pethoalpar/androidtesstwoocr/MainActivity.java
+++ app/src/main/java/com/pethoalpar/androidtesstwoocr/MainActivity.java
@@ -1,13 +1,18 @@
 package com.pethoalpar.androidtesstwoocr;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -36,10 +41,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) this.findViewById(R.id.textView);
-
+        final Activity activity = this;
         this.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},120);
+                }
+                if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},121);
+                }
                 startCameraActivity();
             }
         });
@@ -128,5 +139,22 @@ public class MainActivity extends AppCompatActivity {
         }
         tessBaseAPI.end();
         return retStr;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 120:{
+                if(grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Read permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+            case 121:{
+                if(grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Write permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+            return;
+        }
     }
 }
